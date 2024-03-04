6c4524a1321caab56177b606a2a98aed3d7ef0a5
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-03-04 15:29:45 -0300

    feat: add componet Paragraph

---
+++ src/ParagraphStyle.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const ContentParagraph = styled.p`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  font-size: 2rem;
+  color: var(--third-color);
+`
