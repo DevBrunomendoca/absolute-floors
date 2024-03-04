571887166cb8b6d6f43209a166389b8ffabfa0cf
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-03-04 15:30:37 -0300

    feat: add component Button

---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+import { ContentButton } from "./ButtonStyle"
+
+const Button = ({textButton}) => {
+  return(
+    <ContentButton>{textButton}</ContentButton>
+  )
+}
+
+export default Button
---
+++ src/ButtonStyle.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContentButton = styled.button`
+  padding: 1rem 8rem;
+  background-color: var(--second-color);
+  color: var(--primary-color);
+  font-family: "Cormorant Garamond", serif;
+  font-size: 2rem;
+  font-weight: 300;
+  font-style: normal;
+  border-radius: 5px;
+  border: 2px solid transparent;
+  cursor: pointer;
+`
