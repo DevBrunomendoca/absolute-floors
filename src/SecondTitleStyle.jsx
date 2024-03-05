16a753d2168a2eef960305d04f45755e0b329768
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-03-05 10:30:00 -0300

    feat: add component SecondTitle

---
+++ src/SecondTitleStyle.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const ContentSecondTitle = styled.h2`
+  font-family: "Cormorant Garamond", serif;
+  font-weight: 700;
+  color: var(--second-color);
+  font-size: 4.5rem;
+  text-align: center;
+  line-height: 120%;
+  max-width: 600px;
+`
